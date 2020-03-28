e64a2e0d1bc30cac357c93f067cee3be6b883119
Tomas <tomas_costillachavez@hotmail.com>
2020-03-28 11:59:47 -0300

    Se agregaron ejercicios

--- Ejercicios Visual Studio/ClaseDos/EjercicioOnce/Program.cs
+++ Ejercicios Visual Studio/ClaseDos/EjercicioOnce/Program.cs
@@ -11,6 +11,48 @@ namespace EjercicioOnce
     {
         static void Main(string[] args)
         {
+            /*11.Realizar una clase llamada Validacion que posea un método estático llamado Validar, que posea la
+               siguiente firma: bool Validar(int valor, int min, int max):
+               a.valor: dato a validar
+               b.min y max: rango en el cual deberá estar la variable valor.
+               Pedir al usuario que ingrese 10 números enteros. Validar con el método desarrollado
+               anteriormente que esten dentro del rango - 100 y 100.
+               Terminado el ingreso mostrar el valor mínimo, el valor máximo y el promedio.
+               Nota: Utilizar variables escalares, NO utilizar vectores.*/
+            
+            Console.WriteLine("Ejercicio 11");
+            int iteraciones = 1;
+            int menor = 100;
+            int mayor = 0;
+            int sumar = 0;          
+            
+            do
+            {
+                Console.Write("Ingresa un valor: ");
+                int valorNum = int.Parse(Console.ReadLine());
+
+                //Validacion.Validar(valorNum, -100, 100);
+                if (Validacion.Validar(valorNum, -100, 100) == true)
+                {
+                    sumar += valorNum;
+                    if(valorNum>mayor)
+                    {
+                        mayor = valorNum;
+                    }
+                    if(valorNum<menor)
+                    {
+                        menor = valorNum;
+                    }
+                }
+                iteraciones += 1;
+            } while (iteraciones <= 10);
+            Validacion.MostrarPromedio(sumar, 10);
+            Validacion.Mostrar(menor, mayor);
+    
+
+            
+            
+
 
 
 
--- Ejercicios Visual Studio/ClaseDos/EjercicioOnce/Validacion.cs
+++ Ejercicios Visual Studio/ClaseDos/EjercicioOnce/Validacion.cs
@@ -11,9 +11,29 @@ namespace EjercicioOnce
 
         public static bool Validar(int valor, int min, int max) //En los metodos la primer letra va en mayuscula
         {
-            
+            if (valor >= min && valor <= 100)
+            {
+                Console.WriteLine("Valor correcto!");
+                return true;
+            }
+            else
+                Console.WriteLine("Se detectaron errores!");
+                return false;        
+        }
+
+        public static bool Mostrar(int menor, int mayor) //En los metodos la primer letra va en mayuscula
+        {
+            Console.WriteLine("El menor de los datos ingresados fue: {0}",menor);
+            Console.WriteLine("El mayor de los datos ingresados fue: {0}", mayor);
             return true;
         }
 
+        public static bool MostrarPromedio(int suma, int total) //En los metodos la primer letra va en mayuscula
+        {
+            int promedio = 0;
+            promedio = suma / total;
+            Console.WriteLine("El promedio de los numeros ingresados es: {0}", promedio);
+            return true;
+        }
     }
 }
