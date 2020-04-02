528a66f2f731578e25ca7805120d42ef4e1ec04a
Tomas <tomas_costillachavez@hotmail.com>
2020-04-01 22:06:14 -0300

    Se agregaron ejercicios

---
+++ Ejercicios Visual Studio/Clase_Tres/Ejercicio_16/Alumno.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_16
+{
+    class Alumno
+    {
+        private byte nota1;
+        private byte nota2;
+        private float notafinal;
+        public string apellido;
+        public int legajo;
+        public string nombre;
+
+        public void CalcularFinal()
+        {/* c. El método CalcularFinal deberá colocar la nota del final sólo si las notas 1 y 2 son mayores o
+                iguales a 4, caso contrario la inicializará con -1. Para darle un valor a la nota final utilice
+                el método de instancia Next de la clase Random.*/
+            Random numAleat = new Random();
+            int numero = 0;
+            
+            numero = numAleat.Next(5,10);
+            this.notafinal = numero;
+        }
+
+        public void Estudiar(byte notaUno,byte notaDos)
+        {
+            this.nota1 = notaUno;
+            this.nota2 = notaDos;
+
+            if (nota1 >= 4 && nota2 >= 4)
+            {
+                this.CalcularFinal();
+            }
+            else
+                this.notafinal = -1;
+        }
+
+        public string Mostrar()
+        {
+            /*d.El método Mostrar, expondrá en la consola todos los datos de los alumnos. La nota final se
+                mostrará sólo si es distinto de -1, caso contrario se mostrará la leyenda "Alumno
+                desaprobado".*/
+            string muestra = null;
+            if (this.notafinal != -1)
+            {
+                muestra = "Alumno: " + this.nombre + "Nota 1: " + this.nota1 + "Nota 2: " + this.nota2 + "Nota final: " + this.notafinal;
+
+            }
+            else
+                muestra = "Desaprobado";
+
+            
+            return muestra;
+        }
+
+
+    }
+}
---
+++ Ejercicios Visual Studio/Clase_Tres/Ejercicio_16/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*a.Se pide crear 3 instancias de la clase Alumno (3 objetos) en la función Main. Colocarle
+                nombre, apellido y legajo a cada uno de ellos.
+                b. Sólo se podrá ingresar las notas (nota1 y nota2) a través del método Estudiar.
+                c. El método CalcularFinal deberá colocar la nota del final sólo si las notas 1 y 2 son mayores o
+                iguales a 4, caso contrario la inicializará con -1. Para darle un valor a la nota final utilice
+                el método de instancia Next de la clase Random.
+                d. El método Mostrar, expondrá en la consola todos los datos de los alumnos. La nota final se
+                mostrará sólo si es distinto de -1, caso contrario se mostrará la leyenda "Alumno
+                desaprobado".*/
+
+            Alumno alumno1 = new Alumno();
+            Alumno alumno2 = new Alumno();
+            Alumno alumno3 = new Alumno();
+
+            #region Instanciando Alumnos
+            alumno1.legajo = 123;
+            alumno1.nombre = "Tomas";
+            alumno1.apellido = "Costilla";
+
+            alumno2.legajo = 456;
+            alumno2.nombre = "Pedro";
+            alumno2.apellido = "Guevara";
+
+            alumno3.legajo = 789;
+            alumno3.nombre = "Pablo";
+            alumno3.apellido = "Guevara";          
+            #endregion
+
+
+           //int cantAlumn =0;
+
+            Console.Write("Ingrese Nota 1 para el alumno1: ");
+            byte notaAlum1 = byte.Parse(Console.ReadLine());
+            Console.Write("Ingrese Nota 2 para el alumno1: ");
+            byte notaDosAlum1 = byte.Parse(Console.ReadLine());
+
+            alumno1.Estudiar(notaAlum1, notaDosAlum1);
+            Console.WriteLine(alumno1.Mostrar());
+
+            Console.Write("Ingrese Nota 1 para el alumno2: ");
+            byte notaAlum2 = byte.Parse(Console.ReadLine());
+            Console.Write("Ingrese Nota 2 para el alumno2: ");
+            byte notaDosAlum2 = byte.Parse(Console.ReadLine());
+
+            alumno2.Estudiar(notaAlum2, notaDosAlum2);
+            Console.WriteLine(alumno2.Mostrar());
+
+            
+
+
+        }
+    }
+}
