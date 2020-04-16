f1acb70cb2cd2c1fb91e5892d29158d7147a1c2a
Tomas <tomas_costillachavez@hotmail.com>
2020-04-16 20:44:20 -0300

    Se agrego Ejercicio 31(Terminar)

---
+++ Ejercicios Visual Studio/Ejercicio_31/Entidades/Cliente.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Cliente
+    {
+        private string nombre;
+        private int numero;
+
+        #region Propiedades
+        public string PropNombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+            set
+            {
+                this.nombre = value;
+            }
+        }
+
+        public int PropNum
+        {
+            get
+            {
+                return this.numero;
+            }
+            set
+            {
+                this.numero = value;
+            }
+        }
+        #endregion
+
+        #region Constructores
+        public Cliente(int numero)
+        {
+            this.numero = numero;
+        }
+        public Cliente(int numero, string nombre) : this(numero)
+        {
+            this.nombre = nombre;
+        }
+        #endregion
+
+        #region Sobrecargas
+
+        public static bool operator !=(Cliente c1,Cliente c2)
+        {
+            return !(c1 == c2);
+        }
+        public static bool operator ==(Cliente c1, Cliente c2)
+        {
+            return c1 == c2;
+        }
+        #endregion
+
+
+    }
+}
---
+++ Ejercicios Visual Studio/Ejercicio_31/Entidades/PuestoAtencion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class PuestoAtencion
+    {
+        public enum Puesto
+        {
+            Caja1,Caja2
+        }
+
+        private int numeroActual;
+        private Puesto puesto;
+
+        #region Propiedades
+        public int NumeroActual 
+        {
+            get
+            {
+                return this.numeroActual;
+            }
+        }
+        #endregion
+
+        public bool Atender(Cliente cli)
+        {
+            return true;
+        }
+
+        private PuestoAtencion()
+        {
+            this.numeroActual = 0;
+        }
+
+
+    }
+}
