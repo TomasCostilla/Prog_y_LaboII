c34b67ff7213981a57efcfa25d260d1664f13f99
Tomas <tomas_costillachavez@hotmail.com>
2020-04-16 22:00:11 -0300

    Terminar ejercicio 31

---
+++ Ejercicios Visual Studio/Ejercicio_33/Entidades/Libro.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Libro
+    {
+        private List<string> paginas; //= new List<string>();
+
+        public string this[int i]
+        {
+            get 
+            {
+                if (i >= 0 && i < this.paginas.Count - 1)
+                {
+
+                    return this.paginas[i];
+                }
+                else
+                    return string.Empty;
+                
+                    
+            }
+            set 
+            {
+                //this.paginas = value; 
+            }
+        }
+
+
+    }
+}
