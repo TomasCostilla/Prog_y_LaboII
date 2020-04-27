bfe3661009ee545bb9e681ec1500da86d589bd63
Tom <tomas_costillachavez@hotmail.com>
2020-04-26 22:52:31 -0300

    Se agrego codigo

---
+++ Ejercicios Visual Studio/Ejercicio_34/Entidades/Automovil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Automovil
+    {
+        public enum Colores
+        {
+            Rojo,Blanco,Azul,Gris,Negro
+        }
+
+        public short cantidadRuedas;
+        public short cantidadPuertas;
+        public ConsoleColor colores;
+        public short cantidadMarchas;
+        public int cantidadPasajeros;
+
+
+    }
+}
---
+++ Ejercicios Visual Studio/Ejercicio_34/Entidades/Camion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Camion
+    {
+        public enum Colores
+        {
+            Rojo, Blanco, Azul, Gris, Negro
+        }
+
+
+
+
+
+    }
+}
