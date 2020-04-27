8e98c44597a0c7b11c20d670627b2b70aa148bd4
Tom <tomas_costillachavez@hotmail.com>
2020-04-26 21:16:04 -0300

    Se agrego codigo

--- Ejercicios Visual Studio/Ejercicio_33/Ejercicio_33/Program.cs
+++ Ejercicios Visual Studio/Ejercicio_33/Ejercicio_33/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entidades;
 
 namespace Ejercicio_33
 {
@@ -10,7 +11,8 @@ namespace Ejercicio_33
     {
         static void Main(string[] args)
         {
-
+            Libro Libro1 = new Libro();
+            
 
 
         }
--- Ejercicios Visual Studio/Ejercicio_33/Entidades/Libro.cs
+++ Ejercicios Visual Studio/Ejercicio_33/Entidades/Libro.cs
@@ -10,23 +10,29 @@ namespace Entidades
     {
         private List<string> paginas; //= new List<string>();
 
+
+        public Libro()
+        {
+            this.paginas = new List<string>();
+        }
+
         public string this[int i]
         {
             get 
             {
                 if (i >= 0 && i < this.paginas.Count - 1)
                 {
-
                     return this.paginas[i];
                 }
                 else
-                    return string.Empty;
-                
-                    
+                    return string.Empty;                  
             }
             set 
             {
-                 
+                if(i>this.paginas.Count)
+                {
+                    this.paginas[i] = value;
+                }
             }
         }
 
