a794d0803c4ba482ec128914faf4c07955b16fcc
Tom <tomas_costillachavez@hotmail.com>
2020-04-28 15:26:36 -0300

    Guardado

---
+++ Ejercicios Visual Studio/CentralTelefonica/CentralitaHerencia/Local.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    public class Local : Llamada
+    {
+
+        protected float costo;
+
+        public Local(Llamada llamada,float costo) :this(llamada.NroOrigen,llamada.Duracion,llamada.NroDestino,costo)
+        {
+
+        }
+        public Local(string origen,float duracion,string destino, float costo) :base(duracion,destino,origen)
+        {
+            this.costo = costo;
+        }
+
+        public float CostoLlamada 
+        { 
+            get
+            {
+                return this.costo;
+            }
+        }
+
+
+    }
+}
