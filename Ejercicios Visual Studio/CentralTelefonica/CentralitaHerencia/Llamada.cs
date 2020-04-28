20a5b98eadc22be58e21026f3bbc6c714cc62aaa
Tom <tomas_costillachavez@hotmail.com>
2020-04-28 13:25:58 -0300

    Centralita

---
+++ Ejercicios Visual Studio/CentralTelefonica/CentralitaHerencia/Llamada.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    public class Llamada
+    {
+
+        protected float duracion;
+        protected string nroDestino;
+        protected string nroOrigen;
+
+        #region Propiedades
+        public float Duracion 
+        { 
+            get
+            {
+                return this.duracion;
+            } 
+        }
+        public string NroDestino 
+        { 
+            get
+            {
+                return this.nroDestino;
+            }
+        }
+        public string NroOrigen 
+        { 
+            get
+            {
+                return this.nroOrigen;
+            }
+        }
+        #endregion
+
+    }
+}
