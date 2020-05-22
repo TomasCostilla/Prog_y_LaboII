6c5d77b125d9624174b98252cbef39f6e923b64c
Tomas <tomas_costillachavez@hotmail.com>
2020-05-22 10:55:01 -0300

    Exepciones

---
+++ Ejercicios Visual Studio/Exepciones/Exepciones/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			try
+			{
+                Console.WriteLine("Escriba un numero: ");
+                int x = int.Parse(Console.ReadLine());
+                Console.WriteLine("Escriba un numero: ");
+                int y = int.Parse(Console.ReadLine());
+                int k = x / y;
+            }
+			catch (FormatException e)
+			{
+                Console.WriteLine(e.Message);
+			}
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+
+        }
+    }
+}
