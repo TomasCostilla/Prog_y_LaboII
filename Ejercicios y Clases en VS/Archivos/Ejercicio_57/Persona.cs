6d82cfd124977360c94b9b5492015e9ef6a69fc1
Tom <tomas_costillachavez@hotmail.com>
2020-05-27 22:28:47 -0300

    Archivos-Serializacion

---
+++ Ejercicios y Clases en VS/Archivos/Ejercicio_57/Persona.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Ejercicio_57
+{
+    public class Persona
+    {
+        private string nombre;
+        private string apellido;
+
+        public Persona(string nom,string apellido)
+        {
+            this.nombre = nom;
+            this.apellido = apellido;
+        }
+
+        public static void Guardar(Persona p)
+        {
+            Persona aux = p;
+
+            XmlTextWriter writer;
+            XmlSerializer ser;
+
+            writer = new XmlTextWriter("C:\\Users\\tomas\\Desktop\\", Encoding.UTF8);
+            ser = new XmlSerializer(typeof(aux));
+
+        }
+        public static void Leer()
+        {
+
+        }
+
+
+    }
+}
